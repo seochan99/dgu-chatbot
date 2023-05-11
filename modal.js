a99ce698904bf0e45aba061eef1b3084341cf531
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-05-11 17:20:17 +0900

    ✨ 모달창 구현

---
+++ modal.js
@@ -0,0 +1,9 @@
+function openModal() {
+    const modal = document.getElementById("myModal");
+    modal.classList.add("active");
+}
+
+function closeModal() {
+    const modal = document.getElementById("myModal");
+    modal.classList.remove("active");
+}
